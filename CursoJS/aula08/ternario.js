fafb1ddc38062d5b881603a9aa618dd8abfc16f2
ricardogiorgiano <97055957+ricardogiorgiano@users.noreply.github.com>
2022-01-22 08:23:32 -0300

    DOM e Ternário

---
+++ CursoJS/aula08/ternario.js
@@ -0,0 +1,4 @@
+
+// ternário
+var idade = 17
+var r = idade >= 18 ? 'Maior' : 'Menor'
---
+++ CursoJS/aula09/dados.js
@@ -0,0 +1,4 @@
+var nome = window.prompt('Qual é seu nome?')
+window.alert('É um prazer te conhecer, ' + nome + '!') //concatenação
+        
+
