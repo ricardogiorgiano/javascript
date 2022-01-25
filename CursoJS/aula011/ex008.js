bda11a27c4d3fddd9a6f845a099a840de23d1396
ricardogiorgiano <97055957+ricardogiorgiano@users.noreply.github.com>
2022-01-25 09:57:30 -0300

    ex009

---
+++ CursoJS/aula011/ex008.js
@@ -0,0 +1,7 @@
+var vel = 78
+console.log(`A velcidade do seu carro é ${vel}`)
+if (vel > 60) {
+    console.log(`Você ultrpassou a velocidade permitida. MULTADO!`)
+
+}
+console.log(`Diriga sempre usando cinto de segurança!`)
