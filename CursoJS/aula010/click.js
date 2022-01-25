e846c6cb5a1e3892452e8cd0b6b46276b1aadf42
ricardogiorgiano <97055957+ricardogiorgiano@users.noreply.github.com>
2022-01-25 09:31:53 -0300

    Estudo JavaScript

---
+++ CursoJS/aula010/click.js
@@ -0,0 +1,13 @@
+var a = window.document.getElementById('area')
+       
+        function clicar() {
+            a.innerText = 'Clicou'
+            a.style.background = 'red'        
+        }
+        function entrar() {
+            a.innerText = 'Entrou'
+        }
+        function sair() {
+            a.innerText = 'Saiu'
+            a.style.background = 'green'
+        }
