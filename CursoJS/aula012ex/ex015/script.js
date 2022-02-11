abdf7db640cb270880dfdbdde01dfb7f1e8be0bd
ricardogiorgiano <97055957+ricardogiorgiano@users.noreply.github.com>
2022-02-11 17:49:08 -0300

    margin

--- CursoJS/aula012ex/ex015/script.js
+++ CursoJS/aula012ex/ex015/script.js
@@ -46,6 +46,8 @@ function verificar() {
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
         res.appendChild(img)
+        
+        
     }
     
 
